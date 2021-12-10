bec806afe87909c0bf93375266c53899a3a6e07d
Stephen Bastians <stevejb71@gmail.com>
2021-12-10 21:40:49 +0800

    Day 10, Part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,4 @@
+use std::collections::*;
 use crate::common::*;
 
 pub fn run(part: u8) -> u64 {
@@ -17,19 +18,75 @@ pub fn part1(lines: &Vec<&str>) -> u64 {
   }
   lines.iter()
     .filter_map(|&line| find_first_syntax_error(line))
-    .map(|error| score(error))
+    .map(|result| {
+      if let ParseResult::SyntaxError(ch) = result {
+        score(ch)
+      } else {
+        0
+      }
+    })
     .sum()
 }
 
-fn find_first_syntax_error(line: &str) -> Option<char> {
-  use std::collections::*;
-  let mut stack: VecDeque<char> = VecDeque::new();
-  fn check(stack: &mut VecDeque<char>, expected: char, error: char) -> Option<char> {
+pub fn part2(lines: &Vec<&str>) -> u64 {
+  let mut scores = lines.iter()
+    .filter_map(|&line| find_first_syntax_error(line))
+    .filter_map(|result| {
+      if let ParseResult::Incomplete(stack) = result {
+        Some(autocomplete(&stack))
+      } else {
+        None
+      }
+    })
+    .map(|ac| score_autocompletion(&ac))
+    .collect::<Vec<_>>();
+  let middle_index = (scores.len() - 1) / 2;
+  *scores.select_nth_unstable(middle_index).1
+}
+
+fn score_autocompletion(line: &Vec<char>) -> u64 {
+  fn score_char(ch: char) -> u64 {
+    match ch {
+      ')' => 1,
+      ']' => 2,
+      '}' => 3,
+      '>' => 4,
+      _ => panic!("unhandled")
+    }  
+  }
+  line.iter().fold(0u64, |score, &ch| {
+    score * 5 + score_char(ch)
+  })
+}
+
+fn autocomplete(stack: &VecDeque<char>) -> Vec<char> {
+  stack.iter().map(|ch| {
+    match ch {
+      '(' => ')',
+      '[' => ']',
+      '{' => '}',
+      '<' => '>',
+      _ => panic!("unhandled")
+    }
+  }).collect()
+}
+
+#[derive(Debug, PartialEq)]
+enum ParseResult {
+  SyntaxError(char),
+  Incomplete(VecDeque<char>),
+}
+
+fn find_first_syntax_error(line: &str) -> Option<ParseResult> {
+  let mut stack = VecDeque::new();
+  fn check(stack: &mut VecDeque<char>, expected: char, error: char) -> Option<ParseResult> {
     let actual = stack.pop_front();
-    if actual != Some(expected) {
-      Some(error)
-    } else {
+    if actual == Some(expected) {
       None
+    } else if actual == None {
+      Some(ParseResult::Incomplete(stack.clone()))
+    } else {
+      Some(ParseResult::SyntaxError(error))
     }
   }
   for ch in line.chars() {
@@ -48,11 +105,11 @@ fn find_first_syntax_error(line: &str) -> Option<char> {
       return error;
     }
   }
-  None
-}
-
-pub fn part2(_lines: &Vec<&str>) -> u64 {
-  0
+  if stack.is_empty() {
+    None
+  } else {
+    Some(ParseResult::Incomplete(stack))
+  }
 }
 
 #[cfg(test)]
@@ -60,15 +117,38 @@ mod tests {
   use super::*;
 
   #[test]
-  fn find_first_syntax_error_on_first_line() {
+  fn find_first_syntax_error_on_corrupt_line_returns_syntax_error() {
     let error = find_first_syntax_error("{([(<{}[<>[]}>{[]{[(<()>");
-    assert_eq!(Some('}'), error);
+    assert_eq!(Some(ParseResult::SyntaxError('}')), error);
+  }
+
+  #[test]
+  fn find_first_syntax_error_on_incomplete_line_returns_stack_which_can_be_autocompleted() {
+    let error = find_first_syntax_error("[({(<(())[]>[[{[]{<()<>>");
+    if let Some(ParseResult::Incomplete(stack)) = error {
+      let completion = autocomplete(&stack);
+      assert_eq!(completion, vec!['}', '}', ']', ']', ')', '}', ')', ']']);
+    } else {
+      assert!(false, "{:?}", error);
+    }
   }
 
   #[test]
   fn part1_find_the_syntax_error_score() {
     let lines = sample();
     assert_eq!(26397, part1(&lines));
+  }
+
+  #[test]
+  fn part2_scoring_works() {
+    let score = score_autocompletion(&")}>]})".chars().collect());
+    assert_eq!(5566, score);
+  }
+
+  #[test]
+  fn part2_find_the_autocomplete_score() {
+    let lines = sample();
+    assert_eq!(288957, part2(&lines));
   }
 
   fn sample() -> Vec<&'static str> {
