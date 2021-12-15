f30d00c15f5690d89237dbd467899bf4915653ff
Stephen Bastians <stevejb71@gmail.com>
2021-12-15 19:31:30 +0800

    Day 15: Prep

---
+++ src/day15.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+use crate::common::*;
+
+pub fn run(part: u8) -> u64 {
+  let input_lines = include_str!("../inputs/day15.txt");
+  run_part(part, input_lines, vec_of_str, part1, part2)
+}
+
+pub fn part1(lines: &Vec<&str>) -> u64 {
+  0
+}
+
+pub fn part2(lines: &Vec<&str>) -> u64 {
+  0
+}
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ fn main() {
     12 => day12::run,
     13 => day13::run,
     14 => day14::run,
+    15 => day15::run,
     _ => panic!("Unimplemented")
   };
   let result = run(part);
